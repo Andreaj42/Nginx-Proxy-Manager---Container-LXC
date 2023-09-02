7654804e369aaa5c10767d1c215a03c594e5f7ef
Andréa Joly <59033540+Andreaj42@users.noreply.github.com>
2023-09-02 11:01:37 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 TMP=/tmp/npm_install.sh
-#URL=https://raw.githubusercontent.com/ej52/proxmox-scripts/main/lxc/nginx-proxy-manager/install
-URL=https://raw.githubusercontent.com/Andreaj42/test/main/install
+URL=https://raw.githubusercontent.com/Andreaj42/Nginx-Proxy-Manager---Container-LXC/main/install
 
 if [ "$(uname)" != "Linux" ]; then
   echo "OS NOT SUPPORTED"
