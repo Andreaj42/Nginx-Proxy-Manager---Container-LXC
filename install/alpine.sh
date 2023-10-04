134bfefab6c332556c30190107ff7a4ecd301796
Andreaj42 <andrea.joly1989@gmail.com>
2023-10-04 22:22:18 +0200

    e

--- install/alpine.sh
+++ install/alpine.sh
@@ -177,9 +177,7 @@ cp -r global/* /app/global
 log "Building frontend"
 cd ./frontend
 export NODE_ENV=development
-ls -lah
-cat package.json
-runcmd yarn install
+runcmd yarn install --ignore-scripts -ignore-optional
 runcmd yarn build
 cp -r dist/* /app/frontend
 cp -r app-images/* /app/frontend/images
